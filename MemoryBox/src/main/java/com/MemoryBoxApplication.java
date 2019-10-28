e20519662a3b2e66108a6ebbf29328a9f3f5603e
Benjamin_Edwards <benj.edwrds@gmail.com>
2019-10-28 16:20:25 -0400

    memorybox

---
+++ MemoryBox/src/main/java/com/MemoryBoxApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryBoxApplication.class, args);
+	}
+
+}
